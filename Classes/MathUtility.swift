3225e1a09468b902bec322c5f58d1efd5e64e675
amanK1n <amankonen0799@gmail.com>
2022-02-16 15:14:37 +0530

    Added code

---
+++ Classes/MathUtility.swift
@@ -0,0 +1,24 @@
+//
+//  MathUtility.swift
+//  MathUtilityLib
+//
+//  Created by comviva on 16/02/22.
+//
+
+import Foundation
+public class MathUtility {
+    public init() {}
+   public func add(a: Int, b: Int) -> Int{
+        return a + b
+    }
+    
+   public func multiply(a: Int, b: Int) -> Int{
+        return a * b
+    }
+    
+    func divide(a: Int, b: Int) -> Int{
+        return a / b
+    }
+    
+    
+}
--- Example/MathUtilityLib/ViewController.swift
+++ Example/MathUtilityLib/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
-
+import MathUtilityLib
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let m = MathUtility()
+        var r1 = m.add(a: 11, b: 22)
+        r1 = m.multiply(a: 23, b: 11)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
